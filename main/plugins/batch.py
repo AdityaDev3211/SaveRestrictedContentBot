3e28d1018e05210410b82d4434d175010e6eba77
AdityaDev3211 <162105322+AdityaDev3211@users.noreply.github.com>
2024-03-11 19:56:42 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -1,5 +1,5 @@
-#Tg:MaheshChauhan/DroneBots
-#Github.com/Vasusen-code
+#Tg:Matrix_Botz
+#Github.com/AdityaDev3211
 
 """
 Plugin for both public & private channels!
@@ -81,16 +81,16 @@ async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
         timer = 60
         if i < 25:
-            timer = 5
+            timer = 3
         if i < 50 and i > 25:
+            timer = 5
+        if i < 1000 and i > 50:
             timer = 10
-        if i < 100 and i > 50:
-            timer = 15
         if not 't.me/c/' in link:
             if i < 25:
-                timer = 2
+                timer = 1
             else:
-                timer = 3
+                timer = 2
         try: 
             if not sender in batch:
                 await client.send_message(sender, "Batch completed.")
